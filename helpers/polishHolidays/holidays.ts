9343f0f6ced53d89f2f5781ba8a48000d7341d11
wachcio <wachcio@poczta.fm>
2022-05-26 21:59:59 +0200

    Add public holidays

---
+++ helpers/polishHolidays/holidays.ts
@@ -0,0 +1,31 @@
+import { Holiday } from '../../types';
+
+// 19 lutego – Dzień Nauki Polskiej (ustanowiony w 2020, jako święto państwowe)[20]
+// 1 marca – Narodowy Dzień Pamięci „Żołnierzy Wyklętych” (ustanowiony w 2011, jako święto państwowe)[14]
+// 24 marca – Narodowy Dzień Pamięci Polaków ratujących Żydów pod okupacją niemiecką (ustanowiony w 2018, jako święto państwowe)[17]
+// 14 kwietnia – Święto Chrztu Polski (ustanowione w 2019, jako święto państwowe)[19]
+// 1 maja – święto państwowe, nieformalnie nazywane Świętem Pracy (ustanowione w 1950)[8]
+// 3 maja – Święto Narodowe Trzeciego Maja (ustanowione w 1919[4], ponownie w 1990)[11]
+// 8 maja – Narodowy Dzień Zwycięstwa (ustanowiony 24 kwietnia 2015 roku „w celu upamiętnienia zwycięstwa nad hitlerowskimi Niemcami”)[15]
+// 12 lipca – Dzień Walki i Męczeństwa Wsi Polskiej (ustanowiony w 29 września 2017 jako święto państwowe w „hołdzie mieszkańcom polskich wsi za ich patriotyczną postawę w czasie II wojny światowej”)[16]
+// 1 sierpnia – Narodowy Dzień Pamięci Powstania Warszawskiego (ustanowiony w 2009, jako święto państwowe)[13]
+// 31 sierpnia – Dzień Solidarności i Wolności (ustanowiony w 2005, jako święto państwowe)[12]
+// 19 października – Narodowy Dzień Pamięci Duchownych Niezłomnych (ustanowiony w 2018, jako święto państwowe)[18]
+// 11 listopada – Narodowe Święto Niepodległości (ustanowione w 1937[5], ponownie w 1989)[9]
+// 27 grudnia – Narodowy Dzień Zwycięskiego Powstania Wielkopolskiego (ustanowiony w 2021, jako święto państwowe)[21]
+
+export const publicHolidays: Holiday[] = [
+  { day: 19, month: 2 },
+  { day: 1, month: 3 },
+  { day: 24, month: 3 },
+  { day: 14, month: 4 },
+  { day: 1, month: 5 },
+  { day: 3, month: 5 },
+  { day: 8, month: 5 },
+  { day: 12, month: 7 },
+  { day: 1, month: 8 },
+  { day: 31, month: 8 },
+  { day: 19, month: 10 },
+  { day: 11, month: 11 },
+  { day: 27, month: 12 },
+];
